ad8506a5e60bd7530c1779ce5ce705d5f6e84806
ADITYA DATTA <aditya.datta.aiml25@heritageit.edu.in>
2024-08-01 11:15:18 +0530

    Commiting .c file

---
+++ trail.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+<<<<<<< HEAD
+printf("HELLO GIT!");
+return 0;
+}
+=======
+	print("Welcome to GIT\n");
+	return 0;
+}
+
+>>>>>>> parent of b350800 (Revert "Trying to commit first time")
--- trial.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main(){
-printf("HELLO GIT!");
-return 0;
-}
